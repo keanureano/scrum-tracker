6911dc6ade9acd2c61545fb129057b79e29397f3
andreeeQ <eg.andrequimio@gmail.com>
2023-07-03 14:42:10 +0800

    added dropdown function

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Dropdown from "@/components/Dropdown";
 
 export const metadata = {
   title: "",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><Dropdown/>{children}</body>
     </html>
   );
 }
