2bbc45718fb7b5af20b3a13945c406f4173d42d7
Keanu Reaño <reanokeanu@gmail.com>
2023-07-03 12:38:29 +0800

    add auth credentials route

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (!user) return null;
+
+        return user;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
