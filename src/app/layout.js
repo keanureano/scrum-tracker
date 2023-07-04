8e6d84a391473da0c046a76a53085fc83d63b5fc
Keanu Reaño <reanokeanu@gmail.com>
2023-07-04 09:14:30 +0800

    final refactor

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body><Dropdown/>
+      <body>
+        <Dropdown />
         <SignOut />
         {children}
       </body>
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,7 +1,6 @@
 export default function SignOut() {
   return (
     <>
-      <a href="/api/auth/signin">Sign In</a>
       <a href="/api/auth/signout">Sign Out</a>
       <a href="/api/auth/session">Session</a>
     </>
